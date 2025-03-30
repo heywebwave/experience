dfffb2f2e5c0807051461139901a607cad7e7fc9
Tekdiverse <tekdiverse@gmail.com>
2025-03-30 17:50:44 +0100

    ...

--- static/js/script2.js
+++ static/js/script2.js
@@ -45,12 +45,6 @@ function initCountdowns() {
 
         // Format the countdown text
         element.textContent = `${days}d : ${hours}h : ${minutes}m : ${seconds}s`;
-        const eventDay = `<br>This trip will begin in ${days} days,<br> ${hours} hours, ${minutes} minutes, and ${seconds}<br> seconds. Please check back later.`;
-        
-        const detailsElement = element.parentElement.querySelector('.countdown-details');
-        if (detailsElement) {
-            detailsElement.innerHTML = eventDay;
-        }
 
         // Add highlight animation when minutes or seconds change
         if (seconds === 0) {
@@ -111,26 +105,28 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-function toggleRegister() {
-    const popup = document.getElementById('registerPopup');
+
+function toggleEvent() {
+    const popup = document.getElementById('eventPopup');
     popup.classList.toggle('active');
     document.body.style.overflow = popup.classList.contains('active') ? 'hidden' : '';
 }
 
 // Update the click outside handler
 document.addEventListener('click', function(event) {
-    const popup = document.getElementById('registerPopup');
+    const popup = document.getElementById('eventPopup');
     const isClickInside = popup.contains(event.target);
     const isJoinUsButton = event.target.closest('.btn-dark');
     
     // Only handle click-outside on desktop
     if (window.innerWidth >= 992) {
         if (popup.classList.contains('active') && !isClickInside && !isJoinUsButton) {
-            toggleRegister();
+            toggleEvent();
         }
     }
 });
-document.querySelector('.register-content').addEventListener('click', function(event) {
+// Prevent popup from closing when clicking inside
+document.querySelector('.event-content').addEventListener('click', function(event) {
     event.stopPropagation();
 });
 
